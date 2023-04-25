9b0ead4bd5f70c7b603a83f7f101fa914f1a6fea
DanBd22 <dan.badarau04@gmail.com>
2023-04-25 19:18:21 +0300

    Initial Commit

---
+++ UserDefaults/UserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsApp.swift
+//  UserDefaults
+//
+//  Created by Badarau Dan on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
